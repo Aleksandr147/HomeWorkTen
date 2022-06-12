95263e7c284fe3757dad488e431ce621217dcc63
Aleksandr Belousov <after.game.world@gmail.com>
2022-06-13 04:56:48 +0500

    Add setupView() method.

---
+++ HomeWorkTen/HomeWorkTen/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  HomeWorkTen
+//
+//  Created by Александр Белоусов on 13.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    func setupView() {
+        view.backgroundColor = .systemRed
+    }
+
+}
+
